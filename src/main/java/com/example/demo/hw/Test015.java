79c66b9eb668c8deff47f714a08c784f62c6fd26
sunzhinan <15850748693@163.com>
2022-03-27 01:56:36 +0800

    可信二期 21、22题提交

--- src/main/java/com/example/demo/hw/Test015.java
+++ src/main/java/com/example/demo/hw/Test015.java
@@ -4,4 +4,93 @@ package com.example.demo.hw;
  * 可信二期21题
  */
 public class Test015 {
+
+    private static int index;
+    private static int length;
+    private static StringBuilder stringBuilder = new StringBuilder();
+    private static  boolean flag = true;
+    private static char c[][] = {
+            {'A','A','C','C','F'},
+            {'K','C','D','E','D'},
+            {'A','B','E','S','S'},
+            {'C','F','E','C','A'},
+            {'C','F','E','C','A'}
+
+    };
+
+//    private static char c[][] = {
+//            {'A','C','C','F'},
+//            {'C','D','E','D'},
+//            {'B','E','S','S'},
+//            {'F','E','C','A'}
+//
+//    };
+    private static int strLength;
+    private static char strs[] ;
+
+    private static String fun(){
+
+        String s = "ACCESS";
+
+        strs = s.toCharArray();
+
+        length = c.length;
+        strLength = s.length();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if(c[i][j] == strs[0]){
+                    flag = true;
+                    stringBuilder.append(i + "," + j + ",");
+                    check(i,j,0);
+                    if (stringBuilder.length() > 0){
+                        return stringBuilder.toString();
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static void check(int i,int j,int k){
+        if (!flag){
+            return;
+        }
+
+        index = k+1;
+        if (index == strLength){
+            flag = false;
+            return;
+        }
+
+        if(i < length && j+1 < length && checkOutofBounds(j+1) && c[i][j+1] == strs[k+1]){
+            stringBuilder.append(i + "," + (j+1) + ",");
+            check(i,j+1,k+1);
+        }else if (i+1 < length && j < length && checkOutofBounds(i+1) && c[i+1][j] == strs[k+1]){
+            stringBuilder.append((i+1) + "," + j + ",");
+            check(i+1,j,k+1);
+        }else if (i < length && j-1 < length && checkOutofBounds(j-1) && c[i][j-1] == strs[k+1]){
+            stringBuilder.append(i + "," + (j-1) + ",");
+            check(i,j-1,k+1);
+        }else if (i-1 < length && j < length && checkOutofBounds(i-1) && c[i-1][j] == strs[k+1]){
+            stringBuilder.append((i-1) + "," + j + ",");
+            check(i-1,j,k+1);
+        }else{
+            flag = false;
+            index = 0;
+            stringBuilder.setLength(0);
+        }
+    }
+
+    private static boolean checkOutofBounds(int x){
+        if (x < 0 || x>= length ){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = fun();
+        System.out.println(s.substring(0,s.length()-1));
+    }
 }
--- src/main/java/com/example/demo/hw/Test016.java
+++ src/main/java/com/example/demo/hw/Test016.java
@@ -1,7 +1,47 @@
 package com.example.demo.hw;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * 可信二期22题
  */
 public class Test016 {
+
+    private static int fun(){
+//        Scanner scanner = new Scanner(System.in);
+//        String str = scanner.nextLine();
+//        scanner.close();
+
+        String str = "7 5 9 4 2 6 8 3 5 4 3 9";
+//        String str = "1 2 3 7 1 5 9 3 2 1";
+        String[] s = str.split(" ");
+        int sum = -1;
+
+        int a[] = Arrays.asList(s).stream().mapToInt(Integer::parseInt).toArray();
+        for (int i = 1; i < a.length/2; i++) {
+            int next = i;
+            int k = 1;
+            while (next < a.length){
+                if(next == a.length-1){
+                    if(sum == -1){
+                        sum = k;
+                    }else
+                    {
+                        sum = sum < k ? sum : k;
+                    }
+                    break;
+                }
+                next = a[next] + next;
+                k++;
+            }
+
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fun());
+    }
 }
