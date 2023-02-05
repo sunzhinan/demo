17526880d519cefaedf4a009b20baeb97ecf5f0a
sunzhinan <15850748693@163.com>
2023-02-05 13:09:11 +0800

    初级算法题提交

--- src/main/java/com/example/demo/algorithm/primary/Primary004.java
+++ src/main/java/com/example/demo/algorithm/primary/Primary004.java
@@ -17,12 +17,12 @@ public class Primary004 {
         // 冒泡排序，再比较，但是这个执行会超时
         // 选择排序，再比较,这个执行会超时也会超时
         //因此选择系统自带的sort排序
-//        Arrays.sort(nums);
-//        for (int i = 0; i < nums.length - 2; i++) {
-//            if ((nums[i] ^ nums[i + 1]) == 0) {
-//                return true;
-//            }
-//        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if ((nums[i] ^ nums[i + 1]) == 0) {
+                return true;
+            }
+        }
 
         /**
          * 方法二
