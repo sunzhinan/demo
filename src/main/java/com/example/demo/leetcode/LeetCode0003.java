e21af11d9d30002103c79328807560c59e189e77
sunzhinan <15850748693@163.com>
2023-06-02 08:39:25 +0800

    leetcode基础算法提交

--- src/main/java/com/example/demo/leetcode/LeetCode0003.java
+++ src/main/java/com/example/demo/leetcode/LeetCode0003.java
@@ -10,25 +10,25 @@ import java.util.Map;
  */
 public class LeetCode0003 {
     public static void main(String[] args) {
-        String str = "abcbcde";
+        String str = "abbbcacbb";
         System.out.println(fun(str));
     }
 
-    public static int fun(String str){
-        Map<Character,Integer> map = new HashMap<Character,Integer>();
+    public static int fun(String str) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
         int left = 0;
         int right = 0;
         int result = 0;
-        while (right < str.length()){
+        while (right < str.length()) {
             char c = str.charAt(right);
-            if(map.containsKey(c)){
+            if (map.containsKey(c)) {
                 /**
-                 * 为啥这里用Math.max方法来移动left？思考用例abba
+                 * 为啥这里用Math.max方法来移动left？
                  */
                 left = Math.max(left,map.get(c) +1 );
             }
-            map.put(c,right);
-            result = Math.max(result,right-left+ 1);
+            map.put(c, right);
+            result = Math.max(result, right - left + 1);
             right++;
         }
         return result;
